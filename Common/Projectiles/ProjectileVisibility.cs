3b71d724aadad06a96bee037a1ad396a221a6ab1
Dominic Davies <dominictdavies@gmail.com>
2025-07-26 20:01:06 +0800

    13-projectiles-stop-existing (#15)

--- Common/Projectiles/ProjectileVisibility.cs
+++ Common/Projectiles/ProjectileVisibility.cs
@@ -13,20 +13,29 @@ namespace ProjectilesBeGone.Common.Projectiles
             None
         }
 
-        public static bool BossActive { get; set; } = true;
         public static ProjMode Mode { get; set; } = ProjMode.HostileAndYours;
+        public static bool BossActive { get; set; } = true;
+
+        private static int? _reviveAuraType;
+        private static bool IsReviveAura(int projectileType)
+        {
+            if (_reviveAuraType is null &&
+                ModLoader.TryGetMod("ReviveMod", out Mod mod) &&
+                mod.TryFind("ReviveAura", out ModProjectile aura)) {
+                _reviveAuraType = aura.Type;
+            }
+            return _reviveAuraType is int auraType && projectileType == auraType;
+        }
 
         public override void AI(Projectile projectile)
         {
-            ModLoader.TryGetMod("ReviveMod", out Mod reviveMod);
-            reviveMod.TryFind("ReviveAura", out ModProjectile reviveAura);
-            if (projectile.type == reviveAura.Type || (BossActive && !Main.CurrentFrameFlags.AnyActiveBossNPC)) {
+            if ((BossActive && !Main.CurrentFrameFlags.AnyActiveBossNPC) || IsReviveAura(projectile.type)) {
                 return;
             }
 
-            if (Mode == ProjMode.None ||
-               Mode == ProjMode.Hostile && !projectile.hostile ||
-               Mode == ProjMode.HostileAndYours && !projectile.hostile && projectile.owner != Main.myPlayer) {
+            if ((Mode == ProjMode.None) ||
+                (Mode == ProjMode.Hostile && !projectile.hostile) ||
+                (Mode == ProjMode.HostileAndYours && !projectile.hostile && projectile.owner != Main.myPlayer)) {
                 projectile.hide = true;
             }
         }
